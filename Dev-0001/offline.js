a95686b7d6d4a860ffe3f1eab2652a4d8881aea0
Abolfazl! <109854666+BargmyAppStore@users.noreply.github.com>
2023-06-07 18:34:10 +0330

    Add files via upload

---
+++ Dev-0001/offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1686150040,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"panorama_-sheet0.png",
+		"tiledbackground.png",
+		"untitlejjjjd-sheet0.png",
+		"loading_-sheet0.png",
+		"loading_-sheet1.png",
+		"9patch2.png",
+		"sprite-sheet0.png",
+		"app.png",
+		"9patch.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
